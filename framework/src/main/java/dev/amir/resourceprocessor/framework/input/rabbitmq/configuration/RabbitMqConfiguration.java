cafd44ddad26e1a2d606379eed7bf603711192c8
Amir Aranibar <amir.epam@tripactions.tech>
2023-02-17 00:43:45 +0100

    Get queue name value from properties file in configuration class

--- framework/src/main/java/dev/amir/resourceprocessor/framework/input/rabbitmq/configuration/RabbitMqConfiguration.java
+++ framework/src/main/java/dev/amir/resourceprocessor/framework/input/rabbitmq/configuration/RabbitMqConfiguration.java
@@ -2,11 +2,14 @@ package dev.amir.resourceprocessor.framework.input.rabbitmq.configuration;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfiguration {
+    @Value("${spring.rabbitmq.queues.resource-process}")
+    private String resourceProcessQueue;
 
     @Bean
     public Jackson2JsonMessageConverter converter() {
@@ -15,6 +18,6 @@ public class RabbitMqConfiguration {
 
     @Bean
     public Queue createResourceProcessQueue() {
-        return new Queue("q.resource-process");
+        return new Queue(resourceProcessQueue);
     }
 }
